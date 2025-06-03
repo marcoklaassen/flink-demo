508ddf11c84ea88cc09e09e9e8ee03abd286cc96
Marco Klaassen <mklaasse@redhat.com>
2025-06-03 11:22:11 +0200

    add flink error handling example

---
+++ flink-error-handling/README.md
@@ -0,0 +1,66 @@
+# Flink Error Handling Example
+
+## Local Development & Setup
+
+Install Flink CLI: 
+```
+curl https://dlcdn.apache.org/flink/flink-1.20.1/flink-1.20.1-bin-scala_2.12.tgz \
+    -o flink/bin/flink-1.20.1-bin-scala_2.12.tgz
+tar -xzf flink/bin/flink-1.20.1-bin-scala_2.12.tgz -C flink/bin
+rm flink/bin/flink-1.20.1-bin-scala_2.12.tgz
+```
+
+Install s3 plugin: 
+```
+mkdir flink/bin/flink-1.20.1/plugins/flink-s3-fs-hadoop
+curl https://repo1.maven.org/maven2/org/apache/flink/flink-s3-fs-hadoop/1.20.1/flink-s3-fs-hadoop-1.20.1.jar \
+    -o flink/bin/flink-1.20.1/plugins/flink-s3-fs-hadoop/flink-s3-fs-hadoop-1.20.1.jar
+```
+
+Start minio locally:
+```
+podman run -d --name minio \
+  -p 9000:9000 -p 9001:9001 \
+  -e "MINIO_ACCESS_KEY=minioadmin" \
+  -e "MINIO_SECRET_KEY=minioadmin" \
+  quay.io/minio/minio server /data --console-address ":9001"
+```
+
+Install & Configure Minio CLI: 
+```
+brew install minio/stable/mc
+mc alias set local http://127.0.0.1:9000 minioadmin minioadmin
+```
+
+Create bucket in minio:
+```
+mc mb local/flink-error-handling
+```
+
+Start flink local cluster: 
+```
+export FLINK_CONF_DIR=flink/conf
+export FLINK_PLUGINS_DIR=flink/bin/flink-1.20.1/plugins
+flink/bin/flink-1.20.1/bin/start-cluster.sh
+```
+
+Start flink job:
+```
+flink/bin/flink-1.20.1/bin/flink run -c click.klaassen.flink.FlinkErrorHandling target/flink-error-handling-1.0.jar
+```
+
+Have a look at the buckets with the CLI or the UI: 
+```
+mc ls --recursive local/flink-error-handling
+# http://localhost:9001/browser/flink-error-handling
+
+
+# Get the latest file's content: 
+mc cat local/flink-error-handling/data/$(mc ls --recursive --json local/flink-error-handling/data | jq -r '. | select(.type=="file") | [.time, .key] | @tsv' | sort | tail -n 1 | cut -f2)
+mc cat local/flink-error-handling/$(mc ls --recursive --json local/flink-error-handling/error | jq -r '. | select(.type=="file") | [.time, .key] | @tsv' | sort | tail -n 1 | cut -f2)
+```
+
+Stop cluster:
+```
+flink/bin/flink-1.20.1/bin/stop-cluster.sh
+```
---
+++ flink-error-handling/src/main/java/click/klaassen/flink/FlinkErrorHandling.java
@@ -0,0 +1,107 @@
+package click.klaassen.flink;
+
+// Flink APIs
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.metrics.Counter;
+import org.apache.flink.metrics.MetricGroup;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
+
+// For PDF generation
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+// For Output to S3 or filesystem
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.OutputStream;
+import org.apache.flink.connector.file.sink.FileSink;
+import org.apache.flink.core.fs.Path;
+
+// For error handling and logging
+import java.io.IOException;
+import java.io.Serializable;
+import java.io.StringWriter;
+import java.util.Objects;
+
+// JSON
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+// Logging
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FlinkErrorHandling {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FlinkErrorHandling.class);
+
+    private static final OutputTag<String> errorOutputTag = new OutputTag<>("error-output", Types.STRING);
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<String> input = env.fromElements("valid1", "valid2", "INVALID", "valid3");        
+
+        SingleOutputStreamOperator<String> processed = input.process(new ProcessFunction<String, String>() {
+
+            private transient Counter successCounter;
+            private transient Counter errorCounter;
+            
+            @Override            
+            public void open(Configuration parameters) throws Exception {
+                MetricGroup metricGroup = getRuntimeContext().getMetricGroup();
+                successCounter = metricGroup.counter("success_counter");
+                errorCounter = metricGroup.counter("error_counter");
+            }
+
+            @Override
+            public void processElement(String value, Context ctx, Collector<String> out) {
+                try {
+                    if (value.equals("INVALID")) {
+                        throw new RuntimeException("Invalid Value");
+                    }
+                    out.collect("Processed: " + value);
+                    successCounter.inc();
+                    LOG.info("Row '{}' processed successfully", value);
+                } catch (Exception e) {
+                    errorCounter.inc();
+                    ctx.output(errorOutputTag, "Invalid Row: " + value);
+                    LOG.error("Processing Row '{}' failed", value);
+                }
+            }
+        });
+
+        // Successfully processed (optionally write to S3 bucket with PDF Generation)
+        FileSink<String> sink = FileSink
+        .forRowFormat(new Path("s3a://flink-error-handling/data"), new SimpleStringEncoder<String>("UTF-8"))
+        .build();
+        processed.sinkTo(sink);
+        
+        // Write Errors to S3 bucket
+        DataStream<String> errorStream = processed.getSideOutput(errorOutputTag);
+
+        FileSink<String> errorSink = FileSink
+        .forRowFormat(new Path("s3a://flink-error-handling/errors"), new SimpleStringEncoder<String>("UTF-8"))
+        .build();
+        
+        errorStream.sinkTo(errorSink);        
+
+        env.execute("Error handling with Prometheus and S3");
+    }
+}
